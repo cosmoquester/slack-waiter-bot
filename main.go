c4b6715e5cfb93c16356749fdf0915751e3952cb
psj8252 <psj8252@naver.com>
2021-07-12 21:22:29 +0900

    feat: Reply when bot mentioned

--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// "Bot User OAuth Access Token" which starts with "xoxb-"
+	slack_bot_token := os.Getenv("SLACK_BOT_USER_TOKEN")
+	signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
+
+	api := slack.New(slack_bot_token)
+
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if _, err := sv.Write(body); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if err := sv.Ensure(); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+		}
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+				api.PostMessage(ev.Channel, slack.MsgOptionText("Hello World!", false), slack.MsgOptionTS(ev.TimeStamp))
+			}
+		}
+	})
+
+	fmt.Println("[INFO] Server listening")
+	http.ListenAndServe(":3000", nil)
 }
